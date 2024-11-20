50b2a41d6b291d98591b502b4da4bccc7bff855c
noobix45 <icataalex45@gmail.com>
2024-11-20 15:52:34 +0200

    12th commit: test memory leaks

--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -1,23 +1,2 @@
-#!/usr/bin/bash
-
-# Set the executable name correctly without spaces around '='
-EXECUTABLE_NAME="build/oop"
-
-
-echo "Checking for executable at: $(pwd)/${EXECUTABLE_NAME}"
-ls -l ./"${EXECUTABLE_NAME}"
-
-# Run the executable with Valgrind, saving the output to a file
-valgrind --leak-check=full --track-origins=yes --error-exitcode=0 ./"${EXECUTABLE_NAME}" &> valgrind_output.txt
-tail -f valgrind_output.txt
-
-# Check for memory leaks in the Valgrind output
-if grep -q "definitely lost: 0 bytes" valgrind_output.txt && grep -q "indirectly lost: 0 bytes" valgrind_output.txt; then
-    echo "No memory leaks detected 🎉"
-else
-    echo "Memory leaks detected! 🚨 Ai facut-o de oaie Check the Valgrind output below:"
-    cat valgrind_output.txt
-fi
-
-# Run the test script after Valgrind checks
+cat "${INPUT_FILENAME}" | tr -d '\r' | valgrind --leak-check=full --track-origins=yes --error-exitcode=0 ./"${ZIP_NAME}"/"${EXECUTABLE_NAME}" &
 bash ./scripts/run_test.sh 10 1 2
