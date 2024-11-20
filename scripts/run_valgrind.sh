8dcde96a4aa4753ccacded77e7687e8f6f5ac31e
noobix45 <icataalex45@gmail.com>
2024-11-20 16:03:31 +0200

    12th commit: test memory leaks

--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -1,2 +1,2 @@
-cat "${INPUT_FILENAME}" | tr -d '\r' | valgrind --leak-check=full --track-origins=yes --error-exitcode=0 ./"${ZIP_NAME}"/"${EXECUTABLE_NAME}" &
+valgrind --leak-check=full --track-origins=yes --error-exitcode=0 ./"${ZIP_NAME}"/"${EXECUTABLE_NAME}" &> valgrind_output.txt
 bash ./scripts/run_test.sh 10 1 2
